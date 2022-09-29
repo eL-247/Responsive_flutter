9a1b98581f002d36543c8272dc49e4aaedce9dcd
Israel Hailu <israelhailu67.git@gmail.com>
2022-09-30 01:29:45 +0300

    added the constants folder

---
+++ const/navigation_items.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Map<IconData, String> navItems = {
+  Icons.home_filled: 'home_filled',
+  Icons.query_stats: 'stats',
+  Icons.signal_cellular_alt_rounded: 'signal',
+  Icons.headphones: 'headphones',
+  Icons.train_sharp: 'train',
+};
